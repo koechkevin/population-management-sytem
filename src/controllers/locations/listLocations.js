29b267cbfaffd37d37874c019b9bfe6985bdde67
koechkevin <koechkevin92@gmail.com>
2019-07-11 11:56:27 +0300

    .

--- src/controllers/locations/listLocations.js
+++ src/controllers/locations/listLocations.js
@@ -2,28 +2,29 @@ import { Op } from 'sequelize';
 import models from '../../database/models';
 
 const calculateTotals = async () => {
-  const locationsWithParents = await models.Location
-    .findAll({ where: { parent: { [Op.ne]: null } } });
-  const parentLocations = locationsWithParents.map(each => each.parent);
+  const children = await models.Location.findAll({ where: { parent: { [Op.ne]: null } } });
+  const parentLocations = children.map(each => each.parent);
   const locations = await models.Location.findAll();
   return locations.map((location) => {
     if (parentLocations.includes(location.id)) {
-      const locationChildren = locationsWithParents.filter(child => child.parent === location.id);
+      const locationChildren = children.filter(child => child.parent === location.id);
       const locationMales = locationChildren.map(a => a.males);
       const locationFemales = locationChildren.map(a => a.females);
       const maleTotals = locationMales.reduce((a, b) => a + b);
       const femaleTotals = locationFemales.reduce((a, b) => a + b);
+      const output = {
+        // eslint-disable-next-line max-len
+        name: location.name, id: location.id, parent: location.parent, createdAt: location.createdAt, updatedAt: location.updatedAt,
+      };
       return {
-        name: location.name,
-        id: location.id,
-        male: maleTotals,
-        female: femaleTotals,
-        parent: location.parent,
-        createdAt: location.createdAt,
-        updatedAt: location.updatedAt,
+        ...output, males: maleTotals, females: femaleTotals, totals: maleTotals + femaleTotals,
       };
     }
-    return location;
+
+    return {
+      // eslint-disable-next-line max-len
+      name: location.name, id: location.id, males: location.males, females: location.females, totals: location.males + location.females, parent: location.parent, createdAt: location.createdAt, updatedAt: location.updatedAt,
+    };
   });
 };
 
